f85a3da613673eaf66e58ca0bb7196d3dbbda2e4
Daniel Mwinzi <kavisudaniel@gmail.com>
2023-01-16 22:20:45 +0300

    Collections

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,6 @@ fun main() {
 
     println(name + "\t" + age  + "\t" + married + "\t" + height)
 
-
     println("Enter x:")
     var x  = readLine()
 
@@ -84,6 +83,8 @@ fun main() {
         println(car)
     }
 
+
+
     if ("Audi" in cars){
         println("Found")
     } else {
@@ -95,11 +96,105 @@ fun main() {
     }
 
 
-    //lamda expressions
+    //lamda expressions{variables -> return value|| , method body}
 
-    var lambdaexpr  = {}
+     var  result  = add(5,5)
+
+    var resultlambda = {x:Int,y:Int -> x + y}
+    var printlambds  = { println("Hello lambda") }
+    var stringify = {firstname : String, secondname : String -> println(firstname + secondname)}
+
+    println(result)
+    println(resultlambda(4,4))
+    printlambds()
+    stringify("Daniel", "Mwinzi")
+
+    dosum(7,7) { sum -> println(sum) }
+
+
+    // KOTLIN COLLECTIONS
+    // IMMUTABLE - support only read functionalities
+    // MUTABLE - support both read and write functionalities
+
+    //immutable list
+    var users  = listOf<String>("Daniel", "mwinzi","Ian","njoroge")
+
+    for (user in users){
+        println(user)
+    }
+
+    println(users.asReversed())
+    println(users.subList(1,3))
+
+    //mutable list
+
+    var addusers  = mutableListOf<String>()
+    addusers.add("Mutone")
+    addusers.add("Muttwo")
+    addusers.add("Mutthree")
+    addusers.add("Mutfour")
+    addusers.add("Mutfive")
+    addusers.add("Mutfive")
+
+    println(addusers.subList(1,2))
+    addusers.set(2,"Wasss")
+    for (usr in addusers){
+        println(usr)
+    }
+
+     val arrlst  = ArrayList<String>()
+     arrlst.add("Ajay")
+     arrlst.add("Vijay")
+     arrlst.add("Rohan")
+     arrlst.add("Vijay")
+    arrlst.add("Vijay")
+
+    println(arrlst.size)
+    arrlst.forEach { arr-> println(arr) }
+
+    val iterator  = arrlst.iterator()
+
+    while (iterator.hasNext()){
+        println(iterator.next())
+    }
+
+    //set recoginzes same values
+    val names  = setOf<String>("Dan","Dan","mwinzi")
+
+    names.forEach { println(it) }
+
+    val addnames  = mutableSetOf<String>("Ajay","Krishna","Markx","Ajay ")
+    addnames.forEach { println(it)}
+
+    //maps
+
+    val students = mapOf<Int,String>(1 to "nad",2 to "mwinzi", 3 to "man")
+
+    println(students.entries)
+    println(students.keys)
+
+    val mutstudents  = mutableMapOf<Int,String>()
+    mutstudents.set(1,"mutone")
+    mutstudents.set(2, "muttwo")
+    mutstudents.set(3,"mutthree")
+
+    for (mapstu  in mutstudents){
+        println(mapstu)
+    }
 
 
 
+
+}
+
+fun add(x:Int, y:Int)  : Int{
+    return x + y
+}
+
+
+fun dosum(x: Int, y:Int, sum: (Int) -> Unit){
+
+      var result  = x + y
+       sum(result)
 
 }
