7c997016fc401e35bd7e687d501036bd396996d8
Daniel Mwinzi <kavisudaniel@gmail.com>
2023-01-17 10:11:25 +0300

    Kotlin Basic classes

---
+++ src/main/kotlin/Bankclass.kt
@@ -0,0 +1,17 @@
+class NormalClass {
+
+    var accountno  = 0
+    var name : String? = null
+    var amount : Float  = 0f
+
+    fun deposit(){
+        println("$name ${accountno} You have deposited $amount")
+    }
+
+
+    fun withdraw(){
+        println("$name ${accountno} You have deposited $amount")
+    }
+    
+
+}
